50210c15a4c2ae6da7dc3b48f6fc6db0e2b72688
张旭楠 <zhangxunan@yuanxin-inc.com>
2019-01-31 17:20:39 +0800

    更改首页的轮播图，以及轮播图的一些属性

--- js/banner.js
+++ js/banner.js
@@ -16,6 +16,7 @@ Banner.prototype.init = function(){
         // 如果需要分页器
         pagination: {
             el: '.swiper-pagination',
+            clickable :true,
         },
 
         // 如果需要前进后退按钮
